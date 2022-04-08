8646fa9ced2f464d0e90f6c962ec7ecc5443386d
lyq0916 <3054669628@qq.com>
2022-04-08 15:05:00 +0800

    *

---
+++ src/main/java/com/lyq/eca/pojo/Test.java
@@ -0,0 +1,28 @@
+package com.lyq.eca.pojo;
+
+
+public class Test {
+    private String name;
+    private String value;
+
+    public Test(String value,String name) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
